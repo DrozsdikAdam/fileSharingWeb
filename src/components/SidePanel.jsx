0096d9f1a169dd8222ba3adda203b27fd16c835a
DrozsdikAdam <drozsdik17@gmail.com>
2025-07-21 17:41:03 +0200

    theme toggler aand scrapping the settings page

--- src/components/SidePanel.jsx
+++ src/components/SidePanel.jsx
@@ -3,14 +3,15 @@ import { Link } from "react-router-dom";
 import { FaGear } from "react-icons/fa6";
 import { FaHouse } from "react-icons/fa6";
 import { MdNotes } from "react-icons/md";
+import { ThemeToggler } from "./ThemeToggler";
 
-export const SidePanel = (props) => {
+export const SidePanel = ({ setIsOpen, theme, setTheme }) => {
   return (
     <div className="h-screen p-4 flex flex-col items-center w-72">
       <div className="w-full text-right">
         <button
           className="cursor-pointer text-red-500 hover:scale-115 transition-all duration-200"
-          onClick={() => props.setIsOpen(false)}
+          onClick={() => setIsOpen(false)}
         >
           <IoClose size={30} color="red" />
         </button>
@@ -35,14 +36,8 @@ export const SidePanel = (props) => {
             </Link>
           </li>
         </ul>
-        <div className="list-item list-none">
-          <Link
-            to={"/settings"}
-            className="flex items-center justify-center gap-2"
-          >
-            <FaGear className="hover:rotate-180 transition-all duration-300" />
-            <span title="Beállítások">Beállítások</span>
-          </Link>
+        <div>
+          <ThemeToggler setTheme={setTheme} />
         </div>
       </div>
     </div>
