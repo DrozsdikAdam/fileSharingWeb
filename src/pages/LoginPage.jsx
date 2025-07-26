d4f799e687f8491a3a2c3bb4bcdfd0b65f759c49
DrozsdikAdam <drozsdik17@gmail.com>
2025-07-26 19:35:06 +0200

    login page is done and checks the values for their format errors

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -1,3 +1,129 @@
+import { useRef, useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { useUser } from "../contexts/UserContext";
+
 export const LoginPage = () => {
-  return <div className="">asd</div>;
+  const navigate = useNavigate();
+
+  const [emailError, setEmailError] = useState("");
+  const [passwordError, setPasswordError] = useState("");
+
+  const emailRef = useRef();
+  const passwordRef = useRef();
+
+  const { login } = useUser();
+
+  const validateEmail = (email) => {
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    return emailRegex.test(email);
+  };
+
+  const validatePassword = (password) => {
+    //regex 8 betű 1 nagy 1 kicsi 1 szám 1 speciális karakter
+    const passwRegex =
+      /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/;
+    return passwRegex.test(password);
+  };
+
+  const handleClick = (e) => {
+    e.preventDefault();
+    let isFormValid = true;
+    const email = emailRef.current.value;
+    const password = passwordRef.current.value;
+
+    if (email.trim().length === 0) {
+      setEmailError("Nem lehet üres!");
+      isFormValid = false;
+    } else if (!validateEmail(email)) {
+      setEmailError("Nem megfelelő email formátum!");
+      isFormValid = false;
+    } else setEmailError("");
+
+    if (password.trim().length === 0) {
+      setPasswordError("Nem lehet üres!");
+      isFormValid = false;
+    } else if (!validatePassword(password)) {
+      setPasswordError("Nem megfelelő jelszó formátum!");
+      isFormValid = false;
+    } else setPasswordError("");
+
+    if (isFormValid) {
+      login(email, password);
+      navigate("/");
+    }
+  };
+
+  return (
+    <div className="text-center border-2 border-indigo-900 dark:border-indigo-300 rounded-lg shadow-md scale-110 md:scale-120 lg:scale-130">
+      <div className="text-xl font-bold py-4 border-b-2 border-indigo-900 dark:border-indigo-300 cursor-default">
+        Bejelentkezés
+      </div>
+      <div className="px-6">
+        <form>
+          <div className="flex items-center justify-between flex-col px-3 py-3">
+            <div className="flex items-center justify-between gap-8">
+              <label htmlFor="email" className="font-semibold text-lg">
+                Email:
+              </label>
+              <input
+                type="email"
+                id="email"
+                ref={emailRef}
+                title={emailError}
+                className={` px-1 outline-none border-b-2 ${
+                  emailError === ""
+                    ? "border-indigo-900 dark:border-indigo-300"
+                    : "border-red-500"
+                } `}
+              />
+            </div>
+            {emailError === "" ? null : (
+              <p className="text-red-500 w-full text-left italic text-sm opacity-80 pt-1">
+                {emailError}
+              </p>
+            )}
+          </div>
+
+          <div className="flex items-center justify-between flex-col px-3 pt-3 pb-6">
+            <div className="flex items-center justify-between gap-8">
+              <label htmlFor="password" className="font-semibold text-lg">
+                Jelszó:
+              </label>
+              <input
+                title={passwordError}
+                ref={passwordRef}
+                type="password"
+                id="password"
+                className={` px-1 outline-none border-b-2 ${
+                  passwordError === ""
+                    ? "border-indigo-900 dark:border-indigo-300"
+                    : "border-red-500"
+                } `}
+              />
+            </div>
+
+            {passwordError === "" ? null : (
+              <p className="text-red-500 w-full italic text-left text-sm opacity-80 pt-1">
+                {passwordError}
+              </p>
+            )}
+          </div>
+        </form>
+      </div>
+      <div className="p-3 grid grid-cols-2 border-t-2 gap-3 border-indigo-900 dark:border-indigo-300">
+        <div
+          onClick={() => navigate("/")}
+          className="border-2 cursor-pointer hover:bg-red-400/50 font-semibold text-lg border-indigo-900 dark:border-indigo-300 p-2 rounded-lg"
+        >
+          Vissza
+        </div>
+        <button
+          onClick={handleClick}
+          className="border-2 hover:bg-green-400/50 font-semibold text-lg border-indigo-900 dark:border-indigo-300 p-2 rounded-lg"
+        >
+          Bejelentkezés
+        </button>
+      </div>
+    </div>
+  );
 };
